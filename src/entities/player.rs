5812bf65cd91b03c491b40fcd376d76cd4898640
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2021-03-28 15:59:30 -0300

    refact(Player): move player to entities dir

--- src/entities/player.rs
+++ src/entities/player.rs
@@ -1,6 +1,5 @@
 use rand::{thread_rng, Rng};
 
-
 pub struct Player {
   name: String,
   life: u32,
@@ -18,8 +17,8 @@ impl Player {
 
   pub fn attack(&self) -> u16 {
     let random_value: f64 = thread_rng().gen_range(0.0..1.0);
-    let attack = self.power as f64 * random_value;
-    attack as u16
+    let random_attack = self.power as f64 * random_value;
+    random_attack as u16
   }
 
   pub fn get_name(&self) -> &String {
