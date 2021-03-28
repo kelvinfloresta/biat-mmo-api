7e444f7f01b0558e54a825d62e3b088483a6a52d
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2021-03-28 13:20:19 -0300

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+mod player;
+use player::Player;
+
+fn log(player: &Player){
+  println!("Player");
+  println!("name: {}", player.get_name());
+  println!("life: {}", player.get_life());
+  println!("attack: {}", player.attack());
+}
+
+fn main() {
+  let john = Player::new("John", 100, 10);
+  john.attack();
+  log(&john);
+}
+
+
---
+++ src/player.rs
@@ -0,0 +1,32 @@
+use rand::{thread_rng, Rng};
+
+
+pub struct Player {
+  name: String,
+  life: u32,
+  power: u16
+}
+
+impl Player {
+  pub fn new(name: &str, life: u32, power: u16) -> Player {
+    Player {
+      name: name.to_string(),
+      power: power,
+      life: life
+    }
+  }
+
+  pub fn attack(&self) -> u16 {
+    let random_value: f64 = thread_rng().gen_range(0.0..1.0);
+    let attack = self.power as f64 * random_value;
+    attack as u16
+  }
+
+  pub fn get_name(&self) -> &String {
+    &self.name
+  }
+
+  pub fn get_life(&self) -> u32 {
+    self.life
+  }
+}
