6af3eec13e0e59b70e3d4103d71898f05ede9bab
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2021-03-28 20:26:49 -0300

    refact(WeaponType): use named parameter

--- src/entities/weapon.rs
+++ src/entities/weapon.rs
@@ -3,6 +3,14 @@ pub enum WeaponType {
   Sword
 }
 
+pub struct WeaponCTOR {
+  pub id: String,
+  pub name: String,
+  pub weapon_type: WeaponType,
+  pub power: u16,
+  pub defense: u16,
+}
+
 pub struct Weapon {
   id: String,
   name: String,
@@ -12,13 +20,13 @@ pub struct Weapon {
 }
 
 impl Weapon {
-  pub fn new(id: &str, name: &str, power: u16, weapon_type: WeaponType, defense: u16) -> Weapon {
+  pub fn new(params: WeaponCTOR) -> Weapon {
     Weapon {
-      id: id.to_string(),
-      name: name.to_string(),
-      power: power,
-      weapon_type: weapon_type,
-      defense: defense
+      id: params.id,
+      name: params.name,
+      power: params.power,
+      weapon_type: params.weapon_type,
+      defense: params.defense
     }
   }
 
--- tests/test_unit_weapon.rs
+++ tests/test_unit_weapon.rs
@@ -2,11 +2,17 @@ extern crate biat_mmo_api;
 
 #[cfg(test)]
 mod tests {
-  use biat_mmo_api::entities::weapon::{WeaponType, Weapon};
+  use biat_mmo_api::entities::weapon::{WeaponType, Weapon, WeaponCTOR};
 
   #[test]
   fn should_have_name() {
-    let weapon = Weapon::new("1234", "Knife", 5, WeaponType::Sword, 5);
+    let weapon = Weapon::new(WeaponCTOR {
+      id: String::from("1234"),
+      name: String::from("Knife"),
+      power: 5,
+      defense: 5,
+      weapon_type: WeaponType::Sword
+    });
     assert_eq!(weapon.get_name(), "Knife");
   }
 
